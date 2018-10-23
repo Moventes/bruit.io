e59d61bcf6e7b2edc82c82d3755855ca201092c6
louis amstutz <louis.amstutz@moventes.com>
2018-10-23 16:11:32 +0200

    add httpTool

--- src/bruit-tools/http.ts
+++ src/bruit-tools/http.ts
@@ -7,16 +7,11 @@ export class HttpTool {
   static overrideXMLHttpRequest() {
     (function(open) {
       XMLHttpRequest.prototype.open = function() {
-        this.addEventListener(
-          'readystatechange',
-          function() {
-            console.log('HTTP - ready state = ' + JSON.stringify(this));
-          },
-          false
-        );
-        this.addEventListener('load', function() {
-          // do something with the response text
-          console.log('HTTP - load: ' + this.responseText);
+        const method = arguments[0];
+        HttpTool.logCall(method, arguments[1]);
+        this.addEventListener('load', function(evt: ProgressEvent) {
+          const target = <any>evt.target;
+          HttpTool.logResponse(method, target.responseURL, target.status, target.responseText);
         });
         open.apply(this, arguments);
       };
@@ -26,12 +21,79 @@ export class HttpTool {
   static overrideFetch() {
     (function(fetchP) {
       window.fetch = function() {
-        console.log('fetch call with ', arguments);
-        return fetchP.apply(this, arguments).then(res => {
-          console.log('fetch return ', res);
-          return res;
-        });
+        let req = {
+          method: 'GET',
+          body: undefined,
+          headers: undefined,
+          url: undefined
+        };
+        if (typeof arguments[0] === 'string' && arguments.length > 1) {
+          //first param => url
+          //second param => init obj
+          req.body = arguments[1].body || req.body;
+          req.headers = arguments[1].headers || req.headers;
+          req.method = arguments[1].method || req.method;
+          req.url = arguments[0];
+        } else if (typeof arguments[0] === 'object') {
+          req.headers = arguments[0].headers || req.headers;
+          req.method = arguments[0].method || req.method;
+          req.url = arguments[0].url;
+        } else {
+          req.url = arguments[0];
+        }
+        console.log(arguments);
+        HttpTool.logCall(req.method, req.url, req.headers, req.body);
+        return fetchP
+          .apply(this, arguments)
+          .then(res => {
+            const now = new Date();
+            res
+              .json()
+              .then(jsn => {
+                HttpTool.logResponse(req.method, res.url, res.status, jsn, now);
+              })
+              .catch(() => {
+                HttpTool.logResponse(req.method, res.url, res.status);
+              });
+            return res;
+          })
+          .catch(error => {
+            HttpTool.logResponse(req.method, req.url, -1, typeof error === 'object' ? error.status : error);
+            return Promise.reject(arguments);
+          });
       };
     })(window.fetch);
+  }
+
+  private static logCall(method: string, url: string, header?: any, body?: any) {
+    const requestLog = {
+      type: 'request',
+      timestamp: new Date(),
+      method,
+      url,
+      body,
+      header
+    };
+    HttpTool.logHttp(requestLog);
+  }
+
+  private static logResponse(method: string, url: string, status: any, response?: any, timestamp?: Date) {
+    const requestLog = {
+      type: 'response',
+      timestamp: timestamp || new Date(),
+      method,
+      url,
+      status,
+      response
+    };
+    HttpTool.logHttp(requestLog);
+  }
+
+  private static logHttp(requestLog) {
+    if ((<any>console).http) {
+      (<any>console).http((<any>JSON).decycle(requestLog));
+    } else {
+      console.log(requestLog);
+    }
   }
 }
