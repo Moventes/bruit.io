a4fb70b517f3422b7be32d55ef320eab00bc04df
louis amstutz <louis.amstutz@moventes.com>
2018-10-24 18:07:34 +0200

    modal first part

--- src/components/bruit-modal/bruit-modal.tsx
+++ src/components/bruit-modal/bruit-modal.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Element } from '@stencil/core';
+import { Component, Prop, Element, State } from '@stencil/core';
 import { BruitConfig } from '../../models/bruit-config.model';
 import { ConsoleTool } from '../../bruit-tools/console';
 import { HttpTool } from '../../bruit-tools/http';
@@ -26,6 +26,8 @@ export class BruitModal {
 
   // private properties of bruit-modal class
   private _innerBruitElement: string = '';
+  @State()
+  modalOpened: boolean = false;
 
   componentWillLoad() {
     console.log('bruit started ...');
@@ -37,13 +39,15 @@ export class BruitModal {
   }
 
   newFeedback() {
+    this.modalOpened = false;
     const feedback = new Feedback(this.config.apiKey);
     feedback
       .init()
       .then(() => {
         console.log('feedback is initialized! open form');
-        const res = [{ label: 'test form', value: 'youhoooo', type: 'text' }];
-        return feedback.send(res, this.data, this.dataFn);
+        this.modalOpened = true;
+        // const res = [{ label: 'test form', value: 'youhoooo', type: 'text' }];
+        // return feedback.send(res, this.data, this.dataFn);
       })
       .then(() => {
         console.log('feedback is send!');
@@ -54,8 +58,9 @@ export class BruitModal {
     return (
       <span>
         <a onClick={() => this.newFeedback()} innerHTML={this._innerBruitElement} />
-
-        <div class="modal-wrapper">
+        {/* <div class={'page-wrapper ' + (this.modalOpened ? 'open blur-it' : 'close')} /> */}
+        <p>{this.modalOpened ? 'true' : 'false'}</p>
+        <div class={'modal-wrapper ' + (this.modalOpened ? 'open' : 'close')}>
           <div class="modal">
             <div class="head">
               <a class="btn-close trigger" href="#">
