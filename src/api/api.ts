73363aee2da69d6ef9c5568c8ce518f8467163db
Gwenaël HENOT <gwenael.henot@moventes.com>
2018-10-25 16:15:54 +0200

    added pm2 config + fix wHith typo

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import { FeedbackModel } from './../models/feedback.model';
 export class Api {
   static postFeedback(feedback: FeedbackModel): Promise<void> {
-    console.log(':) send feedback : ', feedback);
+    console.log(`:) send feedback to ${process.env.BRUIT_IO_API_URL}: `, feedback);
     return Promise.resolve();
   }
 }
