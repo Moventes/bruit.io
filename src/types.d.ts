83acca149bc63a19d9617a96240038c711bafb56
louis amstutz <louis.amstutz@moventes.com>
2018-11-06 15:01:01 +0100

    test typings

--- src/types.d.ts
+++
@@ -1,2 +0,0 @@
-/* tslint:disable */
-export { BrtColors } from './models/brt-colors.model';
---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+export * from './components.d';
+
+export * from './models/brt-colors.model';
+export * from './models/brt-config.model';
+export * from './models/brt-error.model';
+export * from './models/brt-field.model';
+export * from './models/brt-labels.model';
+export * from './models/brt-log-levels.model';
